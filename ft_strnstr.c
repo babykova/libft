8424831bb1fac083d704426c96d2bbb7db3e1004
Emerald Hayes <ehayes@e4r12p1.21-school.ru>
2019-03-26 15:13:09 +0300

    poka

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,21 +6,18 @@
 /*   By: ehayes <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/16 14:57:47 by ehayes            #+#    #+#             */
-/*   Updated: 2019/03/24 23:02:58 by ehayes           ###   ########.fr       */
+/*   Updated: 2019/03/26 14:59:39 by ehayes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *str, const char *to_find, size_t n)
+static char	*ft_plus(const char *str, const char *to_find, size_t n, size_t i)
 {
-	size_t	m;
-	size_t	i;
-	int		x;
-	int		y;
+	size_t x;
+	size_t y;
+	size_t m;
 
-	if (to_find[0] == '\0')
-		return ((char *)str);
 	while (n > i && str[i] != '\0')
 	{
 		x = i;
@@ -28,8 +25,11 @@ char	*ft_strnstr(const char *str, const char *to_find, size_t n)
 		if (str[i] == to_find[0])
 		{
 			m = n;
-			while ((m - (y++)) && str[x] && to_find[y] && str[x] == to_find[y])
+			while (m > y && str[x] && to_find[y] && str[x] == to_find[y])
+			{
 				x++;
+				y++;
+			}
 			if (to_find[y] == '\0')
 				return ((char *)(str + i));
 		}
@@ -37,4 +37,14 @@ char	*ft_strnstr(const char *str, const char *to_find, size_t n)
 		i++;
 	}
 	return (0);
+}
+
+char		*ft_strnstr(const char *str, const char *to_find, size_t n)
+{
+	size_t i;
+
+	i = 0;
+	if (to_find[0] == '\0')
+		return ((char *)str);
+	return (ft_plus(str, to_find, n, i));
 }
